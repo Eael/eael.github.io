c1e20d82cf9f6699dae24eb0b8a34d2365d0d90f
Earllinux <earllinux@gmail.com>
2025-11-16 22:22:05 +0000

    Changes to Project page

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 // Updated educationData from your PDF
 const educationData = [
-  { type: 'education', title: 'Bachelor of Science: Mathematics', school: 'Kwame Nkrumah University of Science and Technology', date: 'November 2019' },
-  { type: 'cert', title: 'AWS Certified Cloud Practitioner', date: 'Amazon Web Services' },
-  { type: 'cert', title: 'AWS Solutions Architect - Associate', date: 'Amazon Web Services' },
-  { type: 'cert', title: 'ALX-T Cloud DevOps Engineer', date: 'Udacity' },
-  { type: 'cert', title: 'ALX Software Engineering', date: 'ALX Africa' },
-  { type: 'cert', title: 'Google IT Support', date: 'Coursera' },
-  { type: 'cert', title: 'Google IT Automation with Python', date: 'Coursera' }
+  { type: 'education', title: 'Bachelor of Science: Mathematics', school: 'Kwame Nkrumah University of Science and Technology', date: 'November 2019', link: 'https://www.knust.edu.gh/' },
+  { type: 'cert', title: 'AWS Certified Cloud Practitioner', date: 'Amazon Web Services', link: 'https://www.certmetrics.com/amazon/public/verification.aspx?pid=K3Q2KNXT6EXX2A3Y' },
+  { type: 'cert', title: 'AWS Solutions Architect - Associate', date: 'Amazon Web Services', link: 'https://www.certmetrics.com/amazon/public/verification.aspx?pid=K3Q2KNXT6EXX2A3Y' },
+  { type: 'cert', title: 'ALX-T Cloud DevOps Engineer', date: 'Udacity', link: 'https://www.udacity.com/course/alx-t-cloud-devops-engineer-nanodegree--nd9990' },
+  { type: 'cert', title: 'ALX Software Engineering', date: 'ALX Africa', link: 'https://www.alxafrica.com/' },
+  { type: 'cert', title: 'Google IT Support', date: 'Coursera', link: 'https://www.coursera.org/professional-certificates/google-it-support' },
+  { type: 'cert', title: 'Google IT Automation with Python', date: 'Coursera', link: 'https://www.coursera.org/professional-certificates/google-it-automation-with-python' }
 ];
 
 
@@ -23,6 +23,7 @@ function Education() {
               <h3>{item.title}</h3>
               {item.school && <p><strong>{item.school}</strong></p>}
               <p>{item.date}</p>
+              {item.link && <p><a href={item.link} target="_blank" rel="noopener noreferrer">View Certificate</a></p>}
             </div>
           ))}
         </div>
